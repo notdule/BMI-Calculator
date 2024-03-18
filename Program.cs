e3f136645201ff787e4159d8f6d04dbb5cb93faa
notdule <148584497+notdule@users.noreply.github.com>
2024-03-18 22:52:43 +0100

    Complete Change in Logic

--- Program.cs
+++ Program.cs
@@ -1,33 +1,38 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-namespace BMI Calculator
+namespace BMICalculator
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			// weight / (height*height)
-			string weightInput = Console.ReadLine();
-			int weight = 0;
-			Int32.TryParse(weightInput, out weight);
-			Console.WriteLine("Insert Your Height:");
-			string heightInput = Console.ReadLine();
-			int height = 0; 
-			Int32.TryParse(heightInput, out height);
-			int result = 0;
-
-
-			if (weight != null)
-			{
-				result = weight / (height * height);
-				Console.WriteLine(result);
-			}
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BMI Calculator");
+            Console.WriteLine("**************");
+            
+            Console.Write("First Name : ");
+            string fName = Console.ReadLine();
+            Console.Write("Middle Name : ");
+            string mName = Console.ReadLine();
+            Console.Write("Last Name : ");
+            string lName = Console.ReadLine();
+            Console.Write("Weight(kg) : ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Height(m) : ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            
+            double heightsqr = height * 2;
+            double bmi = weight / heightsqr;
+            
+            Console.WriteLine("BMI : " + bmi);
+            
+            if (bmi < 18.5)
+            {
+                Console.WriteLine("Your BMI Status: Underweight");
+            }
+            
+            
+            
+            
+            
+        }
+    }
 }
-
